09afa49e1c2cdda2087228fc53e67e9d73240423
Rodolfo Batista <rodolfobm2511@gmail.com>
2018-05-21 19:37:41 -0300

    Tabela quantitativa continua discreta funcionando

--- src/tabelagrafica/Continua.java
+++ src/tabelagrafica/Continua.java
@@ -1,41 +1,161 @@
 package tabelagrafica;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Continua extends Discreta{
-    private double[] XIFI, XIXFI, classe;
-    private double H, HZ, K;
+public class Continua {
+    //Atributos.
+    private int N;
+    private int[] FI, FZI;
+    private double[] XI, FR, FZR, XIFI, XIXFI, classe;
+    private double H, HZ, K, media, moda, mediana, M, MZ;
 
 //  Construtores.
     public Continua() {
     }
     public Continua(List roll) {
-        
-        super(roll);
-        
-        XIFI = new double[getXI().length];
-        for (int i = 0; i < getXI().length; i++) {
-            XIFI[i] = getXI()[i]*getFI()[i];
+        //Declaração de variaveis.
+        List<String> aux = new ArrayList<>();
+        aux = roll;
+        for (String item : aux) {
+            item = item.trim();
         }
-        
-        XIXFI = new double[getXI().length];
-        for (int i = 0; i < getXI().length; i++) {
-            XIXFI[i] = Math.pow((getXI()[i] * getMedia()),2) * getFI()[i];
+        List<Double> aux2 = new ArrayList<>();
+        for (String item : aux) {
+            aux2.add(Double.parseDouble(item));
         }
+        aux2.sort(null);
         
-        HZ = getMZ() - getM();
-        
-        K = 1 + (3.33*Math.log(getN()));
-        
+        //Valores que nao fazem parte da tabela
+        N = roll.size();
+        M = aux2.get(0);
+        MZ = aux2.get(aux2.size()-1);
+        HZ = MZ - M;
+        K = 1 + 3.33 * Math.log10(N);
         H = HZ/K;
         
-        classe = new double[getXI().length];
-        for (int i = 0; i < classe.length; i++) {
-            classe[i] = (i+1)*H;
+//      Determinação do valor maximo de cada classe.
+        if((int)K%K == 0) classe = new double[(int)K];
+        else classe = new double[(int)K + 1];
+        classe[0] = M + H;
+        for (int i = 1; i < classe.length; i++) {
+            classe[i] = classe[i-1] + H;
         }
+        
+//      O Xi nesse caso é a media de cada classe (valor maximo)+(valor minimo)/2
+        XI = new double[classe.length];
+        for (int i = 0; i < XI.length; i++) {
+            XI[i] = ((classe[i])+(classe[i] - H))/2;
+        }
+        
+        
+        //Criação dos outros valores da tabela baseados no tamanho de XI.
+        FI = new int[XI.length];
+        FZI = new int[XI.length];
+        FR = new double[XI.length];
+        FZR = new double[XI.length];
+    
+        //Preenchimento dos outros valores da tabela, inicialmente com 0.
+        for (int i = 0; i < XI.length; i++) {
+            FI[i] = 0;
+            FZI[i] = 0;
+            FR[i] = 0;
+            FZR[i] = 0;
+        }
+        
+        //fi
+        int cont=0;
+        for (int i = 0; i < XI.length; i++) {
+            for (Double item : aux2) {
+                if(item >= classe[i]-H && item < classe[i] ){
+                    cont++;
+                } 
+            }
+            FI[i] = cont;
+            cont=0;
+        }
+        //Fi
+        FZI[0] = FI[0];
+//      FZI = FI;
+        for (int i = 1; i < FZI.length; i++) {
+            FZI[i] = FI[i];
+            FZI[i] = FZI[i] + FZI[i-1];
+        }
+        //fr
+        for (int i = 0; i < FR.length; i++) {
+            FR[i] = (double)FI[i]/roll.size()*100;
+        }
+        //Fr
+        FZR[0] = FR[0];
+        for (int i = 1; i < FZR.length; i++) {
+            FZR[i] = FR[i];
+            FZR[i] = FZR[i] + FZR[i-1];
+        }
+//      Xi*Fi
+        XIFI = new double[XI.length];
+        for (int i = 0; i < XI.length; i++) {
+            XIFI[i] = XI[i]*FI[i];
+        }
+//      (Xi-media)²*Fi
+        XIXFI = new double[XI.length];
+        for (int i = 0; i < XI.length; i++) {
+            XIXFI[i] = Math.pow((XI[i] - media),2) * FI[i];
+        }
+        
+//      Outros valores.
+        double sum=0;
+        for (String string : aux) {
+            sum += Double.parseDouble(string);
+        }
+        media = sum/aux.size();
+        
+        moda = 0;
+        for (int i = 0; i < XI.length; i++) {
+            if(FI[i] > moda) moda = XI[i];
+        }
+        
+        mediana = XI[XI.length/2];
+        
     }
 
 //  Acessores.
+    public int getN() {
+        return N;
+    }
+    public void setN(int N) {
+        this.N = N;
+    }
+    public int[] getFI() {
+        return FI;
+    }
+    public void setFI(int[] FI) {
+        this.FI = FI;
+    }
+    public int[] getFZI() {
+        return FZI;
+    }
+    public void setFZI(int[] FZI) {
+        this.FZI = FZI;
+    }
+    public double[] getXI() {
+        return XI;
+    }
+    public void setXI(double[] XI) {
+        this.XI = XI;
+    }
+    public double[] getFR() {
+        return FR;
+    }
+    public void setFR(double[] FR) {
+        this.FR = FR;
+    }
+    public double[] getFZR() {
+        return FZR;
+    }
+    public void setFZR(double[] FZR) {
+        this.FZR = FZR;
+    }
     public double[] getXIFI() {
         return XIFI;
     }
@@ -47,6 +167,12 @@ public class Continua extends Discreta{
     }
     public void setXIXFI(double[] XIXFI) {
         this.XIXFI = XIXFI;
+    }
+    public double[] getClasse() {
+        return classe;
+    }
+    public void setClasse(double[] classe) {
+        this.classe = classe;
     }
     public double getH() {
         return H;
@@ -66,15 +192,40 @@ public class Continua extends Discreta{
     public void setK(double K) {
         this.K = K;
     }
-    public double[] getClasse() {
-        return classe;
+    public double getMedia() {
+        return media;
     }
-    public void setClasse(double[] classe) {
-        this.classe = classe;
+    public void setMedia(double media) {
+        this.media = media;
+    }
+    public double getModa() {
+        return moda;
+    }
+    public void setModa(double moda) {
+        this.moda = moda;
+    }
+    public double getMediana() {
+        return mediana;
+    }
+    public void setMediana(double mediana) {
+        this.mediana = mediana;
+    }
+    public double getM() {
+        return M;
+    }
+    public void setM(double M) {
+        this.M = M;
+    }
+    public double getMZ() {
+        return MZ;
+    }
+    public void setMZ(double MZ) {
+        this.MZ = MZ;
     }
     @Override
     public String toString() {
-        return super.toString()+"Continua{" + "XIFI=" + XIFI + ", XIXFI=" + XIXFI + ", H=" + H + ", HZ=" + HZ + ", K=" + K + '}';
+        return "Continua{" + "N=" + N + ", FI=" + FI + ", FZI=" + FZI + ", XI=" + XI + ", FR=" + FR + ", FZR=" + FZR + ", XIFI=" + XIFI + ", XIXFI=" + XIXFI + ", classe=" + classe + ", H=" + H + ", HZ=" + HZ + ", K=" + K + ", media=" + media + ", moda=" + moda + ", mediana=" + mediana + ", M=" + M + ", MZ=" + MZ + '}';
     }
+    
     
 }
--- src/tabelagrafica/jFrame1.java
+++ src/tabelagrafica/jFrame1.java
@@ -26,6 +26,20 @@ public class jFrame1 extends javax.swing.JFrame {
         jScrollPane3 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField5 = new javax.swing.JTextField();
+        jTextField6 = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        jTextField7 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -88,27 +102,84 @@ public class jFrame1 extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setText("N");
+
+        jTextField1.setText("jTextField1");
+
+        jLabel2.setText("M");
+
+        jTextField2.setText("jTextField2");
+
+        jLabel3.setText("m");
+
+        jTextField3.setText("jTextField3");
+
+        jLabel4.setText("jLabel4");
+
+        jTextField4.setText("jTextField4");
+
+        jLabel5.setText("H");
+
+        jLabel6.setText("K");
+
+        jTextField5.setText("jTextField5");
+
+        jTextField6.setText("jTextField6");
+
+        jLabel7.setText("h");
+
+        jTextField7.setText("jTextField7");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane2)
                     .addComponent(jScrollPane1)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jButton1)
                         .addGap(18, 18, 18)
                         .addComponent(jButton2)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton3)))
+                        .addComponent(jButton3))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField4))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField3)))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 415, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 182, Short.MAX_VALUE))
-                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField5))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel6)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField6))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel7)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField7)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -127,7 +198,30 @@ public class jFrame1 extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel5)
+                        .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6)
+                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel7)
+                    .addComponent(jTextField7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(83, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -222,6 +316,13 @@ public class jFrame1 extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
@@ -229,6 +330,13 @@ public class jFrame1 extends javax.swing.JFrame {
     private javax.swing.JTable jTable1;
     private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField jTextField7;
     // End of variables declaration//GEN-END:variables
     public void preencherTabela(String tipo){
         if(tipo.equals("Tabela quantitativa continua")){
@@ -249,6 +357,13 @@ public class jFrame1 extends javax.swing.JFrame {
                     continua.getXIFI()[i], continua.getXIXFI()[i], 
                     continua.getClasse()[i]});
             }
+            
+            jTextField1.setText(""+continua.getN());
+            jTextField2.setText(""+continua.getMZ());
+            jTextField3.setText(""+continua.getM());
+            jTextField5.setText(""+continua.getHZ());
+            jTextField6.setText(""+continua.getK());
+            jTextField7.setText(""+continua.getH());
      
         }else if(tipo.equals("Tabela quantitativa discreta")){
 //          Tabela quantitativa discreta foi selecionada.
